7b50c94fb7ba36f0baf208aaf6ae650b868223f8
kadir <kadirakgul255@gmail.com>
2022-05-03 10:49:28 +0200

    data preperation

---
+++ data_prep.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
