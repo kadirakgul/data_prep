f905f076b6dd090da0ea0d3069771038a589288d
kadir <kadirakgul255@gmail.com>
2022-07-07 17:23:00 +0200

    example of prep

--- titanic_execution.py
+++ titanic_execution.py
@@ -36,3 +36,57 @@ df.loc[(df['SEX'] == 'female') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngfemal
 df.loc[(df['SEX'] == 'female') & ((df['AGE'] > 21) & (df['AGE']) < 50), 'NEW_SEX_CAT'] = 'maturefemale'
 df.loc[(df['SEX'] == 'female') & (df['AGE'] > 50), 'NEW_SEX_CAT'] = 'seniorfemale'
 
+
+df = df.apply(lambda x: x.fillna(x.mode()[0]) if (x.dtype == "O" and len(x.unique()) <= 10) else x, axis=0)
+
+
+binary_cols = [col for col in df.columns if df[col].dtype not in [int, float] and df[col].nunique() == 2]
+
+for col in binary_cols:
+  df = label_encoder(df, col)
+
+df = rare_encoder(df, 0.01)
+
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+df = one_hot_encoder(df, ohe_cols)
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+num_cols = [col for col in num_cols if "PASSENGERID" not in col]
+useless_cols = [col for col in df.columns if df[col].nunique() == 2 and (df[col].value_counts() / len(df) < 0.01).any(axis=None)]
+df.drop(useless_cols, axis=1, inplace=True)
+
+#Standard scaler
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+#Logistic regression modelini kuralım
+y = df["SURVIVED"]
+X = df.drop(["PASSENGERID", "SURVIVED"], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+
+log_model = LogisticRegression().fit(X_train, y_train)
+
+#Başarı değerlendirmeyi test'e göre yapalım
+y_prob = log_model.predict_proba(X_test)[:, 1]
+y_pred = log_model.predict(X_test)
+
+#Confusion matrix oluşturalım
+def plot_confusion_matrix(y, y_pred):
+    acc = round(accuracy_score(y, y_pred), 2)
+    cm = confusion_matrix(y, y_pred)
+    sns.heatmap(cm, annot=True, fmt=".0f")
+    plt.xlabel('y_pred')
+    plt.ylabel('y')
+    plt.title('Accuracy Score: {0}'.format(acc), size=10)
+    plt.show()
+
+plot_confusion_matrix(y_test, y_pred)
+
+# ROC CURVE
+plot_roc_curve(log_model, X_test, y_test)
+plt.title('ROC Curve')
+plt.plot([0, 1], [0, 1], 'r--')
+plt.show()
+
+#Hedef: AUC'yi artırma (Sonuç: 0.83)
+roc_auc_score(y_test, y_prob)
